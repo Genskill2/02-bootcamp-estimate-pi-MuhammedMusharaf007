2e1ce83ec3f1705ae76cec46da2d9d03a791f42a
Muhammed Musharaf <mohammedmusharafnilambur@gmail.com>
2021-05-30 00:11:01 +0530

    Wallis and monte_carlo function added for pi estimation

--- estimate.py
+++ estimate.py
@@ -1,6 +1,30 @@
 import math
 import unittest
+import random
 
+
+def wallis(n):
+	ans=1
+	for i in range(1,n+1):
+		ans *= ((4*(i**2))/(4*(i**2)-1))
+	return ans*2
+	
+
+def monte_carlo(n):
+	in_circle=0
+	total=0
+	for i in range(n):
+		x= random.random()
+		y= random.random()
+		dist =((x**2)+(y**2))**0.5
+		if dist<=1:
+			in_circle+=1
+			total+=1
+		else:
+			total+=1
+	return (4*in_circle/total);
+	
+	
 class TestWallis(unittest.TestCase):
     def test_low_iters(self):
         for i in range(0, 5):
